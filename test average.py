869eafa2e4c0a02fc0de6f0b48d6bdd8a6f75ac6
nicoleczerepak <71734073+nicoleczerepak@users.noreply.github.com>
2020-10-22 12:57:48 -0400

    Create test average.py

---
+++ test average.py
@@ -0,0 +1,12 @@
+score1=0
+score2=0
+total=0
+average=0
+scorecount=0
+
+score1 = int(input("Please enter a test score"))
+score2= int(input("Please enter a test score"))
+
+average= (score1+score2)/2
+
+print=(average)
