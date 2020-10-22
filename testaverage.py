d373fc8716fa02c0f7152e541b5ea7d8469a5325
nicoleczerepak <71734073+nicoleczerepak@users.noreply.github.com>
2020-10-22 12:52:25 -0400

    Create testaverage.py

---
+++ testaverage.py
@@ -0,0 +1,13 @@
+numberofscore - 0
+score = 0
+score2= 0
+total= 0
+average= 0 
+scorecount= 0
+
+score1= int(input("Please enter a test score"))
+score2= int(input("Please enter a test score"))
+
+average= (score1+score2)/2
+
+print(average)
